4199d13c1fa5ad3e1ecd5c5b2f5500a88fe95006
Stefano Beretta <beretta.stefano.7@gmail.com>
2019-10-24 11:09:40 +0200

    Add files via upload

---
+++ DictionaryMetadataProvider/Mvc/DictionaryMetadataProvider.cs
@@ -0,0 +1,164 @@
+﻿using ClientDependency.Core;
+using NPoco;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Mvc;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace FlyingFox.DictionaryMetadataProviderV8.Mvc
+{
+    [TableName("cmsDictionary")]
+    public class UmbracoDictionaryItem
+    {
+        [Column("key")]
+        public string Key { get; set; }
+    }
+
+    public class DictionaryMetadataProvider : DataAnnotationsModelMetadataProvider
+    {
+        private readonly IUmbracoContextFactory _context;
+
+        public UmbracoHelper UmbracoHelper
+        {
+            get
+            {
+                var umbracoHelper = Umbraco.Web.Composing.Current.UmbracoHelper;
+                return umbracoHelper;
+            }
+        }
+        protected override ModelMetadata CreateMetadata(IEnumerable<Attribute> attributes, Type containerType,
+            Func<object> modelAccessor, Type modelType, string propertyName)
+        {
+            var metadata = base.CreateMetadata(attributes, containerType, modelAccessor, modelType, propertyName);
+
+            if (containerType != null && propertyName != null)
+            {
+
+                
+                string key = string.Format("{0}.{1}", containerType.Name.Replace("ViewModel", "").Replace("Model", ""), propertyName);
+                string altKey = string.Format("{0}", propertyName);
+                //get model specific data
+                var displayName = LookupDictionaryValue(key, metadata.DisplayName);
+
+
+                //specific model is empty
+                if (string.IsNullOrEmpty(displayName))
+                {
+                    displayName = LookupDictionaryValue(altKey, null);
+                    if (string.IsNullOrEmpty(displayName))
+                    {
+                        return metadata;
+                    }
+                }
+
+
+                metadata.DisplayName = displayName;
+                metadata.Watermark = metadata.DisplayName;
+                metadata.NullDisplayText = LookupDictionaryValue(key + ".NullDisplayText", metadata.NullDisplayText);
+
+
+                var requiredFormat = LookupDictionaryValue("RequiredFormat", null);
+
+                foreach (var attribute in attributes)
+                {
+                    if (attribute is ValidationAttribute)
+                    {
+                        string attributeName = attribute.GetType().Name;
+
+                        if (attributeName.EndsWith("Attribute"))
+                            attributeName = attributeName.Substring(0, attributeName.Length - 9);
+
+                        string attributeDictKey = string.Format("{0}.{1}", key, attributeName);
+
+                        var validationAttribute = (ValidationAttribute)attribute;
+
+                        if (validationAttribute.ErrorMessageResourceName == null)
+                        {
+                            string errorMessage = LookupDictionaryValue(attributeDictKey, null);
+
+                            if (errorMessage != null)
+                            {
+                                validationAttribute.ErrorMessage = errorMessage;
+                            }
+                            else
+                            {
+                                errorMessage = LookupDictionaryValue(string.Format("{0}.{1}", altKey, attributeName), null);
+                                if (errorMessage != null)
+                                {
+                                    validationAttribute.ErrorMessage = errorMessage;
+                                }
+                            }
+
+
+                            if (validationAttribute is RequiredAttribute && !string.IsNullOrEmpty(requiredFormat))
+                            {
+
+                                validationAttribute.ErrorMessage = string.Format(requiredFormat,
+                                    metadata.DisplayName);
+
+                            }
+
+
+                        }
+                    }
+
+                }
+            }
+
+            return metadata;
+        }
+
+        private static Dictionary<string, string> _dic;
+        private Dictionary<string, string> GlobalDictionary
+        {
+            get
+            {
+                if (_dic == null)
+                {
+                    using (var scope = Current.ScopeProvider.CreateScope())
+                    {
+                        var items = scope.Database.Fetch<UmbracoDictionaryItem>("select * from cmsDictionary");
+                        _dic = items.ToDictionary(x => x.Key, x => x.Key);
+                    }
+                }
+
+                return _dic;
+
+            }
+        }
+        protected virtual string LookupDictionaryValue(string dictKey, string defaultValue)
+        {
+            if (GlobalDictionary.ContainsKey(dictKey))
+            {
+                var value = UmbracoHelper.GetDictionaryValue(dictKey);
+                if (!string.IsNullOrEmpty(value))
+                {
+                    return value;
+                }
+                return defaultValue;
+            }
+            else
+            {
+                return defaultValue;
+            }
+            //if (umbraco.cms.businesslogic.Dictionary.DictionaryItem.hasKey(dictKey))
+            //{
+            //    return library.GetDictionaryItem(dictKey);
+            //}
+            //return defaultValue;
+        }
+
+        public void UpdateCache(IDictionaryItem item)
+        {
+            if (!GlobalDictionary.ContainsKey(item.ItemKey))
+            {
+                GlobalDictionary.Add(item.ItemKey, item.ItemKey);
+            }
+        }
+    }
+}
---
+++ DictionaryMetadataProvider/Startup/StartupApplicationEventHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Mvc;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Services;
+using Umbraco.Core.Services.Implement;
+
+namespace FlyingFox.DictionaryMetadataProviderV8.Startup
+{
+    public class DictionaryMetadataProviderComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            
+            composition.Components().Append<DictionaryMetadataProviderComponent>();
+        }
+    }
+
+    public class DictionaryMetadataProviderComponent : IComponent
+    {
+
+        public void Initialize()
+        {
+            ModelMetadataProviders.Current = new Mvc.DictionaryMetadataProvider();
+            LocalizationService.SavedDictionaryItem += LocalizationService_SavedDictionaryItem;
+        }
+        private void LocalizationService_SavedDictionaryItem(ILocalizationService sender, global::Umbraco.Core.Events.SaveEventArgs<global::Umbraco.Core.Models.IDictionaryItem> e)
+        {
+            
+            if (ModelMetadataProviders.Current is Mvc.DictionaryMetadataProvider provider)
+            {
+                foreach (var item in e.SavedEntities)
+                {
+                    provider.UpdateCache(item);
+                }
+            }
+        }
+        public void Terminate()
+        {
+            
+        }
+    }
+}
